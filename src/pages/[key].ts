312f72fa067875ff9bfe6609ded2667f32c83d8b
daniel lambarri <dalambarri@gmail.com>
2024-02-07 20:02:47 -0300

    fix no key found return

--- src/pages/[key].ts
+++ src/pages/[key].ts
@@ -5,8 +5,13 @@ export const GET: APIRoute = async ({ params, redirect }) => {
 
   const urlKey = params.key
 
-  const [key] = await redis.keys(`*:${urlKey}`)
-  const data = await redis.get(key)
+  const key = await redis.keys(`*:${urlKey}`)
+
+  if(key.length === 0){
+    return new Response('No se encontró ninguna dirección con ese hash',{status: 400})
+  }
+  
+  const data = await redis.get(key[0])
   if(data){
    const urlToRedirect = JSON.parse(data).longUrl
     return redirect(urlToRedirect, 302)
